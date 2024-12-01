910278b87751fb22e6322204f41238ea8250ebe6
Damien LaRocque <phicoltan@gmail.com>
2024-12-01 13:18:43 -0500

    Day 01 - Part 1 in Rust

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,32 @@
 advent_of_code::solution!(1);
-
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let numbers: Vec<_> = input
+        .lines()
+        .map(|c| {
+            c.split_whitespace()
+                .map(|n| n.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<i32>>>();
+    let lists = advent_of_code::vectranspose(numbers);
+
+    let mut vec_a: Vec<i32> = lists[0].clone();
+    let mut vec_b = lists[1].clone();
+
+    vec_a.sort();
+    vec_b.sort();
+
+    let diff: Vec<i32> = vec_a.into_iter().zip(vec_b).map(|(a, b)| (a - b)).collect();
+
+    Some(
+        diff.iter()
+            .map(|&n| n.abs())
+            .into_iter()
+            .map(|n| n as u32)
+            .sum(),
+    )
+
+    // Some(diff)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
