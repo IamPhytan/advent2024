b36d00bdc729a30648986adf1f14d200d4b75155
Damien LaRocque <phicoltan@gmail.com>
2024-12-02 01:42:24 -0500

    Day 02 - Part 01

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,7 +1,42 @@
 advent_of_code::solution!(2);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let report_diff: Vec<_> = input
+        .lines()
+        .map(|report| {
+            report
+                .split_whitespace()
+                .map(|level| level.parse::<i32>().unwrap())
+                .collect::<Vec<i32>>()
+                .windows(2)
+                .map(|s| s[1] - s[0])
+                .collect()
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    let same_sign: Vec<bool> = report_diff
+        .iter()
+        .map(|report| {
+            report
+                .iter()
+                .map(|x| x.signum())
+                .all(|x| x != 0 && x == report[0].signum())
+        })
+        .collect();
+
+    let safe_diff: Vec<bool> = report_diff
+        .iter()
+        .map(|report| report.iter().map(|x| x.abs()).all(|x| (1..4).contains(&x)))
+        .collect();
+
+    Some(
+        same_sign
+            .iter()
+            .zip(safe_diff)
+            .filter(|(&x, y)| x && *y)
+            .count() as u32,
+        // .collect::<bool>()
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
