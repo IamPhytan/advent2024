8bcaaf7073a922afebc20bfe2207b1c96137b4c0
Damien LaRocque <phicoltan@gmail.com>
2024-12-02 12:19:32 -0500

    Day 2 - Part 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,46 +1,64 @@
+use std::fmt::Debug;
+
 advent_of_code::solution!(2);
 
+fn is_safe_report(report: &Vec<i32>) -> bool {
+    let report_diff = report.windows(2).map(|s| s[1] - s[0]).collect::<Vec<i32>>();
+
+    let same_sign = report_diff
+        .iter()
+        .map(|x| x.signum())
+        .all(|x| x != 0 && x == report_diff[0].signum());
+
+    let safe_diff = report_diff
+        .iter()
+        .map(|x| x.abs())
+        .all(|x| (1..4).contains(&x));
+
+    safe_diff && same_sign
+}
+
+fn combinations<T: Clone + Debug>(vec: &[T]) -> Vec<Vec<T>> {
+    let mut combinations = vec![vec.to_vec()];
+    for i in 0..vec.len() {
+        let mut combination = vec.to_vec();
+        combination.remove(i);
+        combinations.push(combination);
+    }
+    combinations
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    let report_diff: Vec<_> = input
+    let reports: Vec<_> = input
         .lines()
         .map(|report| {
             report
                 .split_whitespace()
                 .map(|level| level.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>()
-                .windows(2)
-                .map(|s| s[1] - s[0])
                 .collect()
         })
         .collect::<Vec<Vec<i32>>>();
 
-    let same_sign: Vec<bool> = report_diff
-        .iter()
-        .map(|report| {
-            report
-                .iter()
-                .map(|x| x.signum())
-                .all(|x| x != 0 && x == report[0].signum())
-        })
-        .collect();
-
-    let safe_diff: Vec<bool> = report_diff
-        .iter()
-        .map(|report| report.iter().map(|x| x.abs()).all(|x| (1..4).contains(&x)))
-        .collect();
-
-    Some(
-        same_sign
-            .iter()
-            .zip(safe_diff)
-            .filter(|(&x, y)| x && *y)
-            .count() as u32,
-        // .collect::<bool>()
-    )
+    Some(reports.iter().filter(|&x| is_safe_report(x)).count() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let reports: Vec<_> = input
+        .lines()
+        .map(|report| {
+            report
+                .split_whitespace()
+                .map(|level| level.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect::<Vec<Vec<i32>>>();
+
+    let valid_reports: Vec<_> = reports
+        .iter()
+        .filter(|rep| combinations(rep).iter().any(|comb| is_safe_report(comb)))
+        .collect();
+
+    Some(valid_reports.len() as u32)
 }
 
 #[cfg(test)]
@@ -56,6 +74,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
