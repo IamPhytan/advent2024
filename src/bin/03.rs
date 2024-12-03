b3f89d0b47b87a2ea2033a5d4b44414a7b70b9f2
Damien LaRocque <phicoltan@gmail.com>
2024-12-03 17:22:30 -0500

    Day 03 - Part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -18,8 +18,47 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(products.sum())
 }
 
+fn is_disabled(position: usize, ranges: &Vec<Vec<&usize>>) -> bool {
+    ranges.iter().any(|c| &position > c[0] && &position < c[1])
+}
+
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let re_dont = Regex::new(r"don't\(\)").unwrap();
+    let re_do = Regex::new(r"do\(\)").unwrap();
+
+    let dont_pos: Vec<_> = re_dont.find_iter(input).map(|m| m.start()).collect();
+    let do_pos: Vec<_> = re_do.find_iter(input).map(|m| m.start()).collect();
+    let inlen = input.len();
+
+    let disabled_ranges: Vec<_> = dont_pos
+        .iter()
+        .map(|st| {
+            [
+                st,
+                do_pos
+                    .iter()
+                    .filter(|val| val >= &st)
+                    .min()
+                    .unwrap_or(&inlen),
+            ]
+            .to_vec()
+        })
+        .collect();
+
+    let re_mul = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let products = re_mul
+        .captures_iter(input)
+        .filter(|c| !is_disabled(c.get(0).unwrap().start(), &disabled_ranges))
+        .map(|c| c.extract::<2>())
+        .map(|(_, v)| {
+            v.iter()
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .map(|v| v[0] * v[1]);
+
+    Some(products.sum())
 }
 
 #[cfg(test)]
@@ -35,6 +74,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(48));
     }
 }
