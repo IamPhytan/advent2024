4b2d0016978307d71a0f040786835091d3d8eacb
Damien LaRocque <phicoltan@gmail.com>
2024-12-03 15:41:52 -0500

    Day 03 - Part 1

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,7 +1,21 @@
+use regex::Regex;
+
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let products = re
+        .captures_iter(input)
+        .map(|c| c.extract::<2>())
+        .map(|(_, v)| {
+            v.iter()
+                .map(|n| n.parse::<u32>().unwrap())
+                .collect::<Vec<u32>>()
+        })
+        .map(|v| v[0] * v[1]);
+
+    Some(products.sum())
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +29,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(161));
     }
 
     #[test]
