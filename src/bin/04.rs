a712afaf8de96ec7e360466a25327efb557d7247
Damien LaRocque <phicoltan@gmail.com>
2024-12-04 18:35:31 -0500

    WIP on Part 1

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,64 @@
 advent_of_code::solution!(4);
 
+fn count_occurences(grid: &Vec<String>) -> u32 {
+    grid.iter()
+        .map(|s| {
+            let lr = s.rmatches("XMAS").collect::<Vec<&str>>().len();
+            let rl = s.rmatches("SAMX").collect::<Vec<&str>>().len();
+            lr + rl
+        } as u32)
+        .sum()
+}
+
+fn to_string_grid(grid: &Vec<Vec<char>>) -> Vec<String> {
+    grid.iter()
+        .map(|r| r.into_iter().collect::<String>())
+        .collect()
+}
+
+fn to_diagonal_grid(grid: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let n = grid.len();
+    let mut diag: Vec<Vec<char>> = vec![];
+    for ridx in 0..(2 * n) {
+        let mut row: Vec<char> = vec![];
+        for x in 0..n {
+            if x > ridx {
+                row.push('.');
+                continue;
+            }
+
+            let y = ridx - x;
+            if (x >= n) || (y >= n) {
+                continue;
+            } else {
+                row.push(grid[x][ridx - x]);
+            }
+        }
+        diag.push(row);
+    }
+    diag
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let grid: Vec<Vec<char>> = input
+        .lines()
+        .map(|r| r.to_string().chars().collect())
+        .collect();
+
+    let horiz_sgrid = to_string_grid(&grid);
+    let horiz: u32 = count_occurences(&horiz_sgrid);
+
+    let vertic_grid = advent_of_code::vectranspose(grid.clone());
+    let vertic_sgrid: Vec<String> = to_string_grid(&vertic_grid);
+    let vertic: u32 = count_occurences(&vertic_sgrid);
+
+    let diag_tl_sgrid = to_string_grid(&to_diagonal_grid(&grid));
+    let diag_tl = count_occurences(&diag_tl_sgrid);
+    let diag_bl_sgrid = to_string_grid(&&to_diagonal_grid(&vertic_grid));
+    let diag_bl = count_occurences(&diag_bl_sgrid);
+
+    println!("{} {}", grid.len(), grid[0].len());
+    Some(horiz + vertic + diag_tl + diag_bl)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +72,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(18));
     }
 
     #[test]
