79f4d08800eceab8ba7efbe8f5bc9cfaf4fd4281
Damien LaRocque <phicoltan@gmail.com>
2024-12-05 19:56:16 -0500

    Create a function for input parsing

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,11 +1,31 @@
 advent_of_code::solution!(5);
 use std::cmp::Ordering;
 
-fn get_middle_page(pages: &Vec<i32>) -> u32 {
+fn parse_input(input: &str) -> (Vec<Vec<u32>>, Vec<Vec<u32>>) {
+    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
+
+    let rules: Vec<Vec<u32>> = rules_str
+        .lines()
+        .map(|s| s.to_string())
+        .into_iter()
+        .map(|l| l.split("|").map(|n| n.parse::<u32>().unwrap()).collect())
+        .collect();
+
+    let updates: Vec<Vec<u32>> = updates_str
+        .lines()
+        .map(|s| s.to_string())
+        .into_iter()
+        .map(|l| l.split(",").map(|n| n.parse::<u32>().unwrap()).collect())
+        .collect();
+
+    (rules, updates)
+}
+
+fn get_middle_page(pages: &Vec<u32>) -> u32 {
     return pages.get((pages.len() - 1) / 2).unwrap().to_owned() as u32;
 }
 
-fn is_valid_update(update: &Vec<i32>, rules: &Vec<Vec<i32>>) -> bool {
+fn is_valid_update(update: &Vec<u32>, rules: &Vec<Vec<u32>>) -> bool {
     let applied_rules: Vec<_> = rules
         .iter()
         .filter(|&rule| rule.iter().all(|r| update.contains(&r)))
@@ -20,7 +40,7 @@ fn is_valid_update(update: &Vec<i32>, rules: &Vec<Vec<i32>>) -> bool {
     true
 }
 
-fn rule_order(a: i32, b: i32, rules: &Vec<Vec<i32>>) -> Ordering {
+fn rule_order(a: u32, b: u32, rules: &Vec<Vec<u32>>) -> Ordering {
     for rule in rules.clone() {
         let rule_tup = (rule[0], rule[1]);
         if (a, b) == rule_tup {
@@ -34,28 +54,14 @@ fn rule_order(a: i32, b: i32, rules: &Vec<Vec<i32>>) -> Ordering {
     return Ordering::Equal;
 }
 
-fn sort_update(update: &Vec<i32>, rules: &Vec<Vec<i32>>) -> Vec<i32> {
+fn sort_update(update: &Vec<u32>, rules: &Vec<Vec<u32>>) -> Vec<u32> {
     let mut sorted_upd = update.clone();
     sorted_upd.sort_by(|&a, &b| rule_order(a, b, rules));
     sorted_upd
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
-
-    let rules: Vec<Vec<i32>> = rules_str
-        .lines()
-        .map(|s| s.to_string())
-        .into_iter()
-        .map(|l| l.split("|").map(|n| n.parse::<i32>().unwrap()).collect())
-        .collect();
-
-    let updates: Vec<Vec<i32>> = updates_str
-        .lines()
-        .map(|s| s.to_string())
-        .into_iter()
-        .map(|l| l.split(",").map(|n| n.parse::<i32>().unwrap()).collect())
-        .collect();
+    let (rules, updates) = parse_input(input);
 
     let correct_updates: Vec<_> = updates
         .iter()
@@ -66,21 +72,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let (rules_str, updates_str) = input.split_once("\n\n").unwrap();
-
-    let rules: Vec<Vec<i32>> = rules_str
-        .lines()
-        .map(|s| s.to_string())
-        .into_iter()
-        .map(|l| l.split("|").map(|n| n.parse::<i32>().unwrap()).collect())
-        .collect();
-
-    let updates: Vec<Vec<i32>> = updates_str
-        .lines()
-        .map(|s| s.to_string())
-        .into_iter()
-        .map(|l| l.split(",").map(|n| n.parse::<i32>().unwrap()).collect())
-        .collect();
+    let (rules, updates) = parse_input(input);
 
     let sorted_updates: Vec<_> = updates
         .iter()
