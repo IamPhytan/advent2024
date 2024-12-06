84bd9c790226885faad2f072885c5f7ffe7f3396
Damien LaRocque <phicoltan@gmail.com>
2024-12-06 01:25:00 -0500

    WIP on Day 06

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,6 +1,54 @@
 advent_of_code::solution!(6);
 
+use array2d::Array2D;
+
+fn turn(guard: char) -> char {
+    match guard {
+        '^' => '>',
+        '>' => 'v',
+        'v' => '<',
+        '<' => '^',
+        _ => todo!(),
+    }
+}
+
+fn parse_input(input: &str, direction: char) -> (Array2D<char>, (usize, usize)) {
+    let lines: Vec<String> = input
+        .lines()
+        .into_iter()
+        .map(|line| line.to_owned())
+        .collect();
+
+    let coords: (usize, usize) = *lines
+        .iter()
+        .enumerate()
+        .filter_map(|(i, row)| match row.find(direction) {
+            Some(n) => Some((i, n)),
+            None => None,
+        })
+        .collect::<Vec<_>>()
+        .first()
+        .unwrap();
+
+    let maze = Array2D::from_rows(
+        &lines
+            .iter()
+            .map(|line| line.chars().collect())
+            .collect::<Vec<_>>(),
+    )
+    .unwrap();
+
+    (maze, coords)
+}
+
 pub fn part_one(input: &str) -> Option<u32> {
+    let mut direction: char = '^';
+    let (maze, coords) = parse_input(input, direction);
+
+    println!("{:?}", coords);
+
+    direction = turn(direction);
+
     None
 }
 
