ce4948e7a1a49548c5511e7c0ed02ba876a4b6de
Damien LaRocque <phicoltan@gmail.com>
2024-12-07 23:56:18 -0500

    Day 06 - It's working :tada:

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,6 +1,9 @@
 // Inspired by https://gist.github.com/icub3d/ad5ffceed8a5170725ea389340b4f6b7
 advent_of_code::solution!(6);
 
+use fxhash::FxHashSet;
+use rayon::prelude::*;
+
 use std::{
     collections::{HashMap, HashSet},
     hash::Hash,
@@ -65,13 +68,45 @@ impl Maze {
 
     fn show_maze(&self) {
         for row in self.grid.as_rows() {
-            println!("{:?}", row);
+            println!("{}", row.iter().collect::<String>());
         }
         println!("===========");
     }
 
     fn get(&self, point: &Point) -> Option<&char> {
         self.grid.get(point.y as usize, point.x as usize)
+    }
+}
+
+fn find_loop(maze: &Maze, start: &Point, obstacle: &Point) -> Option<()> {
+    // If we put an obstacle on the next position, we should return to the same position with same heading, or go off the map
+    let mut map = maze.clone();
+    if map.get(&obstacle) == Some(&'.') {
+        let _ = map.grid.set(obstacle.y as usize, obstacle.x as usize, '#');
+    } else {
+        return None;
+    }
+
+    // Run the loop
+    let mut current_pos = *start;
+    let mut current_head = Direction::UP;
+    let mut seen = FxHashSet::default();
+    loop {
+        // Try to move. If not possible, turn right
+        let next_pos = current_pos.next(&current_head);
+        match map.get(&next_pos) {
+            Some(&'#') => {
+                current_head = current_head.turn_guard();
+            }
+            None => return None,
+            _ => {
+                current_pos = next_pos;
+            }
+        }
+        if seen.contains(&(current_pos, current_head)) {
+            return Some(());
+        }
+        seen.insert((current_pos, current_head));
     }
 }
 
@@ -110,9 +145,45 @@ fn parse_input(input: &str) -> (Array2D<char>, Point) {
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let (mut grid, mut current_pos) = parse_input(input);
     let mut heading: Direction = Direction::UP;
+    let (grid, mut current_pos) = parse_input(input);
+    let maze = Maze::new(&grid);
 
+    let mut directions: HashMap<Point, Vec<Direction>> = HashMap::new();
+
+    let mut seen = HashSet::new();
+    seen.insert(current_pos);
+    loop {
+        directions.entry(current_pos).or_default().push(heading);
+
+        // Move if we can
+        let next_pos = current_pos.next(&heading);
+        match maze.get(&next_pos) {
+            Some(&'#') => {
+                heading = heading.turn_guard();
+            }
+            None => break,
+            _ => {
+                current_pos = next_pos;
+                seen.insert(current_pos);
+            }
+        }
+    }
+
+    println!(
+        "{} / {}",
+        directions.values().map(|v| v.len()).sum::<usize>(),
+        maze.grid.num_elements()
+    );
+
+    // maze.show_maze();
+
+    Some(seen.len() as u32)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut heading: Direction = Direction::UP;
+    let (grid, mut current_pos) = parse_input(input);
     let maze = Maze::new(&grid);
     let original_pos = current_pos;
 
@@ -137,15 +208,23 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     }
 
-    Some(seen.len() as u32)
-}
+    let now = std::time::Instant::now();
+    let obstacles = directions
+        .par_iter()
+        .flat_map(|(point, directions)| {
+            directions
+                .iter()
+                .filter_map(|&direction| {
+                    find_loop(&maze, &original_pos, &point.next(&direction))
+                        .map(|_| point.next(&direction))
+                })
+                .collect::<Vec<_>>()
+        })
+        .collect::<HashSet<Point>>();
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let mut guard: Direction = Direction::UP;
-    let (mut maze, mut coords) = parse_input(input);
+    println!("p2: {} ({:?})", obstacles.len(), now.elapsed());
 
-    // Not enough time for this
-    Some(0)
+    Some(obstacles.len() as u32)
 }
 
 #[cfg(test)]
@@ -161,6 +240,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
