149a21443014dfcb56ca433e94f64ac0c1732831
Damien LaRocque <phicoltan@gmail.com>
2024-12-21 22:44:52 -0500

    Fix Day 11 - Done

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,66 +1,67 @@
-use std::collections::HashMap;
+// Inspired by https://github.com/bozdoz/advent-of-code-2024/blob/main/day-11/src/main.rs
+use std::{collections::HashMap, hash::Hash};
 
 advent_of_code::solution!(11);
 
-fn blink(stones: HashMap<u128, u128>) -> HashMap<u128, u128> {
-    let mut blinked: HashMap<u128, u128> = HashMap::new();
+fn parse_input(input: &str) -> Vec<usize> {
+    input
+        .split_whitespace()
+        .map(|n| n.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn blink(stones: HashMap<usize, usize>) -> HashMap<usize, usize> {
+    let mut blinked: HashMap<usize, usize> = HashMap::new();
+
     for (stone, cnt) in stones {
         if stone == 0 {
-            *blinked.entry(1).or_insert(0) += 1;
+            *blinked.entry(1).or_insert(0) += cnt;
         } else {
-            let n_digits = stone.checked_ilog10().unwrap_or(0) + 1;
+            let n_digits = stone.ilog10() as usize + 1;
             if n_digits % 2 == 0 {
                 // Split number
                 let div = n_digits / 2;
-                let b = stone % 10_u128.pow(div);
-                let a = stone / 10_u128.pow(div);
+                let b = stone % 10_usize.pow(div as u32);
+                let a = stone / 10_usize.pow(div as u32);
                 // Insert both numbrs in blinked
-                *blinked.entry(a).or_insert(0) += 1;
-                *blinked.entry(b).or_insert(0) += 1;
+                *blinked.entry(a).or_insert(0) += cnt;
+                *blinked.entry(b).or_insert(0) += cnt;
             } else {
-                *blinked.entry(stone * 2024).or_insert(0) += 1;
+                *blinked.entry(stone * 2024).or_insert(0) += cnt;
             }
         }
     }
     return blinked;
 }
 
-fn parse_input(input: &str) -> Vec<u64> {
-    input
-        .split_whitespace()
-        .map(|n| n.parse::<u64>().unwrap())
-        .collect()
-}
-
 pub fn part_one(input: &str) -> Option<u64> {
     let stones = parse_input(input);
-    let mut stone_cnt: HashMap<u128, u128> = stones.iter().fold(HashMap::new(), |mut map, &s| {
+    let mut stone_cnt: HashMap<usize, usize> = stones.iter().fold(HashMap::new(), |mut map, &s| {
         *map.entry(s.into()).or_insert(0) += 1;
         map
     });
-    // let mut stone_cnt: HashMap<u64, u128> = HashMap::new();
-    // for stone in stones {
-    //     match stone_cnt.get(&stone) {
-    //         Some(count) => {
-    //             stone_cnt.insert(stone, count + 1);
-    //         }
-    //         None => {
-    //             stone_cnt.insert(stone, 1);
-    //         }
-    //     }
-    //     println!("{}", stone);
-    // }
 
     let n_steps = 25;
-    for i in 0..(n_steps + 1) {
+    for _ in (0..).take(n_steps) {
+        // println!("{} {:?}", i, stone_cnt);
         stone_cnt = blink(stone_cnt);
-        println!("{} {:?}", i, stone_cnt.values().sum::<u128>());
     }
-    Some(stone_cnt.values().sum::<u128>() as u64)
+    Some(stone_cnt.values().sum::<usize>() as u64)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let stones = parse_input(input);
+    let mut stone_cnt: HashMap<usize, usize> = stones.iter().fold(HashMap::new(), |mut map, &s| {
+        *map.entry(s.into()).or_insert(0) += 1;
+        map
+    });
+
+    let n_steps = 75;
+    for _ in (0..).take(n_steps) {
+        // println!("{} {:?}", i, stone_cnt);
+        stone_cnt = blink(stone_cnt);
+    }
+    Some(stone_cnt.values().sum::<usize>() as u64)
 }
 
 #[cfg(test)]
